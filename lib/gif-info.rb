0c824dda09948c52be53abd1d4537ea823e348cf
Martin Kozák <martinkozak@martinkozak.net>
2011-03-09 00:24:33 +0100

    library in fact finished

--- lib/gif-info.rb
+++ lib/gif-info.rb
@@ -22,6 +22,10 @@ require "gif-info/blocks/trailer"
 
 class GifInfo
 
+    ############################################################
+    ## STATIC PART
+    ############################################################
+
     ##
     # Defines structure of the block head.
     #
@@ -50,6 +54,30 @@ class GifInfo
     #
     
     def self.parse(io)
+        __parse(io) do |block|
+        
+            # Saves position of beginning of the next block set by
+            # inner parser
+            position = io.pos               
+            
+            # Yields block and allows application seek trough the file 
+            # according to its needs
+            yield block
+            
+            # Seeks to position at beginning of the next block
+            io.seek(position)
+            
+        end
+    end
+    
+    
+    private
+    
+    ##
+    # Just performs parsing of the GIF file.
+    #
+    
+    def self.__parse(io)
         if io.kind_of? String
             io = StringIO::new(io)
         end
@@ -61,8 +89,9 @@ class GifInfo
         yield lsd = Blocks::LogicalScreenDescriptor::new(io)
 
         # Global Color Table
-        if lsd.header.data.packed.data.global_color_table
-            size = lsd.header.data.packed.data.global_color_table_size
+        packed = lsd.header.data.packed.data
+        if packed.global_color_table
+            size = packed.global_color_table_size
             yield Blocks::GlobalColorTable::new(io, size)
         end
 
@@ -112,6 +141,267 @@ class GifInfo
                     raise Exception::new("Invalid format: 0x21, 0x2C or 0x3B expected, but 0x" << header.block_introducer.to_s(16).upcase << " found at position " << io.pos.to_s << ".")
             end
         end
+    end
+    
+    
+    public
+    
+    ##
+    # Considers stream, performs analyzing and returns info object
+    # as result.
+    #
+    # @param [IO] io IO object with GIF data
+    # @return [GifInfo] info object
+    #
+    
+    def self.analyze_io(io)
+        self::new(io)
+    end
+
+    ##
+    # Analyzes string with GIF data and returns info object.
+    #
+    
+    def self.analyze_string(string)
+        self::analyze_io(StringIO::new(string))
+    end
+
+    ##
+    # Analyzes file and returns info object back.
+    # 
+    # @param [String] filename file path
+    # @return [GifInfo] info object
+    #
+    
+    def self.analyze_file(filename)
+        File::open(filename, "rb") do |io|
+            self.analyze_io(io)
+        end
+    end
+
+
+    ############################################################
+    ## INSTANCE PART
+    ############################################################
+    
+    ##
+    # Type of the file. If it isn't +:GIF+, it means, file isn't
+    # valid GIF gile.
+    #
+    # @return [Symbol] always +:GIF+
+    #
+    
+    attr_reader :type
+    @type
+    
+    ##
+    # Version of the GIF standard. Can be +:87a+ or +?89a+.
+    # @return [Symbol] GIF standard version
+    #
+    
+    attr_reader :version
+    @version
+    
+    ##
+    # Width of the image. (Width of the canvas.)
+    # @return [Integer] width
+    #
+    
+    attr_reader :width
+    @width
+    
+    ##
+    # Height of the image. (Height of the canvas.)
+    # @return [Integer] height
+    #
+    
+    attr_reader :height
+    @height
+    
+    ##
+    # Pixel aspect ratio. See standard what it is.
+    # @return [Integer] pixel aspect ration
+    #
+    
+    attr_reader :pixel_aspect_ratio
+    @pixel_aspect_ratio
+    
+    ##
+    # Color resolution in number of colors which can be encoded 
+    # according to global table. If local tables found, it's +nil+.
+    #
+    # @return [Integer] color count
+    #
+    
+    attr_reader :color_resolution
+    @color_resolution
+    
+    ##
+    # Comments inside the file. GIF file can theorethically contain
+    # unlimtited number of the comment blocks, so it returns array.
+    #
+    # @return [Array] array with comments
+    #
+    
+    attr_reader :comments
+    @comments
+    
+    ##
+    # Contains images count. More images in one file are characteristics
+    # for the animated GIF files.
+    #
+    # @return [Integer] images count
+    #
+    
+    attr_reader :images_count
+    @images_count
+    
+    ##
+    # Indicates animation is cyclic.
+    #
+    
+    @cyclic
+
+    ##
+    # Indicates, file is animated.
+    #
+    
+    @animated
+    
+    ##
+    # Indicates, at least one image in file has some color transparent.
+    #
+    
+    @transparent
+
+    ##
+    # Indicates, at least one image in file is rendered interlaced.
+    #
+    
+    @interlaced
+    
+    ##
+    # Constructor.
+    # @param [IO] io IO object with GIF data
+    #
+    
+    def initialize(io)
+        @comments = [ ]
+        @images_count = 0
+        @cyclic = false
+        @animated = false
+        @transparent = false
+        @interlaced = false
+        
+        self.consider! io
+    end
+    
+    ##
+    # Considers IO object, analyzes it and sets result ot the object.
+    # @param [IO] io IO object with GIF data
+    #
+    
+    def consider!(io)
+        self.class::parse(io) do |block|
+        
+            # Header Block
+            if block.kind_of? GifInfo::Blocks::HeaderBlock
+                header = block.header.data
+                @type = header.signature.to_sym         # type
+                @version = header.version.to_sym        # version
+
+            # Logical Screen Descriptor
+            elsif block.kind_of? GifInfo::Blocks::LogicalScreenDescriptor
+                header = block.header.data
+                packed = header.packed.data
+                
+                @width = header.canvas_width            # width
+                @height = header.canvas_height          # height
+                
+                value = header.pixel_aspect_ratio       # pixel aspect ratio
+                value = nil if value <= 0               #
+                @pixel_aspect_ratio = value             #
+                
+                if packed.global_color_table            # color resolution
+                    @color_resolution = 2 ** (packed.global_color_table_resolution + 1)
+                end
+                
+            # Comments
+            elsif block.kind_of? GifInfo::Blocks::CommentExtension
+                @comments << block.body.data            # comments
+                
+            # Image Descriptor
+            elsif block.kind_of? GifInfo::Blocks::ImageDescriptor
+                packed = block.header.data.packed.data 
+                
+                @images_count += 1                                 # images count
+                @animated = true if @images_count == 2             # animated
+                @interlaced |= packed.interlace                    # interlaced
+                
+                if packed.local_color_table and (not @color_resolution.nil?) and (packed.local_color_table_size != @color_resolution)
+                    @color_resolution = nil                         # resets color resolution if local color table found
+                end                                                 # and global and local aren't equivalent
+                
+            # Application Extension
+            elsif block.kind_of? GifInfo::Blocks::ApplicationExtension
+                header = block.header.data
+                if (header.application_identifier == "NETSCAPE") and (header.application_authentication_code == "2.0")
+                    @cyclic = true                                  # cyclic
+                end
+                
+            # Graphics Control Extension
+            elsif block.kind_of? GifInfo::Blocks::GraphicsControlExtension
+                packed = block.header.data.packed.data
+                @transparent |= packed.transparent_color            # transparent
+                
+            end
+            
+        end
+
+        ##
+        # Indicates animation is cyclic. In strange GIF files can be set
+        # although file isn't animated and contains single image only.
+        #
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # 
+        
+        def cyclic?
+            @cyclic
+        end
+         
+        ##
+        # Indicates file is animated. It can be set to +true+ although
+        # animation isn't run because it's set if file contains 
+        # more images.
+        #
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # 
+        
+        def animated?
+            @animated
+        end
+        
+        ##
+        # Indicates, at least one image in file has some color 
+        # transparent. It of sure doesn't mean, image is transparent.
+        # It only means, transparency is turned on.
+        #
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # 
+        
+        def transparent?
+            @transparent
+        end
+
+        ##
+        # Indicates, at least one image in file is rendered interlaced.
+        # @return [Boolean] +true+ if yes, +false+ in otherwise
+        # 
+        
+        def interlaced?
+            @interlaced
+        end
         
     end
+    
 end
